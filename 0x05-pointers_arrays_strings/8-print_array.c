a441ba775031b1e9f312233987d705223afe4fff
Richkidcode <kamaufelix741@gmail.com>
2023-10-16 13:51:20 -0700

    new

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - pritns an array
+ * @a:array
+ * @n: size of array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+
+
