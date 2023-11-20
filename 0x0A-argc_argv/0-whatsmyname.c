f17b49ced522d9330fbbefaaacb78c6adb0dd77f
Richkidcode <kamaufelix741@gmail.com>
2023-11-20 11:00:56 -0800

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+// 0-whatsmyname.c
+
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    (void)argc; // Unused parameter, suppresses compiler warning
+
+    printf("%s\n", argv[0]);
+
+    return 0;
+}
+
